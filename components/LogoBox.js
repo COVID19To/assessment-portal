62a5281cbfe921fe7a9b44854e8403a8de8c4a0f
Khadem Avinoor Alam <demon_avi@hotmail.com>
2020-03-27 18:46:38 -0400

    google default signin replaced with google oauth

--- components/LogoBox.js
+++ components/LogoBox.js
@@ -1,13 +1,14 @@
-const centerImage = {
+const centerImage = () => ({
   display: "block",
   margin: "auto",
   width: "150px",
   padding: "20px"
-};
+});
 
 const LogoBox = props => (
   <div style={props.containerStyle || {}}>
-    <img src="/assets/logo.png" style={Object.assign(centerImage, props.imageStyle || {})} />
+    {console.log(props.style) || ""}
+    <img src="/assets/logo.png" style={Object.assign(centerImage(), props.imageStyle || {})} />
   </div>
 );
 
--- pages/login.js
+++ pages/login.js
@@ -1,91 +1,28 @@
-import TextField from "@material-ui/core/TextField";
-import FormControlLabel from "@material-ui/core/FormControlLabel";
-import Checkbox from "@material-ui/core/Checkbox";
-import Link from "@material-ui/core/Link";
-import Grid from "@material-ui/core/Grid";
-import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
 import SignUpLoginLayout from "../components/SignUpLoginLayout";
-import CustomButton from "../components/CustomButton";
+import { GoogleLogin } from "react-google-login";
+import Router from "next/router";
 
-const useStyles = makeStyles(theme => ({
-  paper: {
-    marginTop: theme.spacing(4),
-    display: "flex",
-    flexDirection: "column",
-    alignItems: "center"
-  },
-  avatar: {
-    margin: theme.spacing(1),
-    backgroundColor: theme.palette.secondary.main
-  },
-  form: {
-    width: "100%", // Fix IE 11 issue.
-    marginTop: theme.spacing(1)
-  },
-  submit: {
-    margin: theme.spacing(3, 0, 2)
+export default function Login() {
+  const responseGoogle = response => {
+    if (response && response.error) console.error("Invalid Login");
+    else {
+      console.log('typeof response', typeof response);
+      console.log('response: ', response && response.error);
+      Router.replace('/');
+    }
   }
-}));
-
-const centerDiv = {
-  margin: 'auto',
-  width: 'fit-content',
-  display: 'block'
-};
-
-export default function Doctor() {
-  const classes = useStyles();
-
   return (
     <SignUpLoginLayout>
       <Container component="main" maxWidth="xs">
-        <div className={classes.paper}>
-          <form className={classes.form} noValidate>
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              id="username"
-              label="Username"
-              name="username"
-              autoComplete="username"
-              autoFocus
-            />
-            <TextField
-              variant="outlined"
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <div style={centerDiv}>
-              <CustomButton size="large" name="Log In" onClick={() => 0} />
-            </div>
-            <Grid container>
-              <div
-                style={{
-                  margin: "10px auto",
-                  display: "block",
-                  width: "fit-content"
-                }}
-              >
-                <Link href="mailto:user@example.com" variant="body2">
-                  {"Don't have an account? Contact Us"}
-                </Link>
-              </div>
-            </Grid>
-          </form>
-        </div>
+        <GoogleLogin
+          clientId="13064702254-8jmahupmn4dcoa4vgbg8qhjj737kupm5.apps.googleusercontent.com"
+          buttonText="Login"
+          onSuccess={responseGoogle}
+          onFailure={responseGoogle}
+          cookiePolicy={"single_host_origin"}
+          isSignedIn={true}
+        />
       </Container>
     </SignUpLoginLayout>
   );
