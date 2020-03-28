a3c2e00da545e25a1d351debaca2cbd8a77eea15
Khadem Avinoor Alam <demon_avi@hotmail.com>
2020-03-28 16:50:15 -0400

    code refined

--- constants/authorization.js
+++ constants/authorization.js
@@ -1,24 +1,19 @@
 // Utilities
 import Router from "next/router";
 
-export const google_authorization = {
-  key: "covid19to.api.token",
+const google_authorization = {
+  key: "covid19to.api.token"
 };
 
-export const is_authorized = () => {
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem(google_authorization.key);
+const is_authorized = () =>
+  typeof window !== "undefined" &&
+  localStorage.getItem(google_authorization.key);
 
-    if (token) {
-      return true;
-    }
-  }
-  return false;
-}
-
-export const logout = () => {
-  if (typeof window !== 'undefined') {
+const logout = () => {
+  if (typeof window !== "undefined") {
     localStorage.removeItem(google_authorization.key);
     Router.push("/login");
   }
-}
+};
+
+export { google_authorization, is_authorized, logout };
